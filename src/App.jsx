400ca4019bbaefba30fff1dee052c46b77362d66
khardeaditya <khardeaditya240@gmail.com>
2025-03-09 15:42:50 +0530

    Buttons Added

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,29 @@ function App() {
                 <button className="btn" style={{backgroundColor:"indigo" }} onClick={() => setColor('black')}>
                     indigo
                 </button>
-              
+                <button className="btn" style={{backgroundColor:"violet"}} onClick={() => setColor('violet')}>
+                    violet</button>
+                <button className="btn" style={{backgroundColor:"orange"}} onClick={() => setColor('orange')}>
+                    orange
+                </button>
+                <button className="btn" style={{backgroundColor:"tomato"}} onClick={() => setColor('tomato')}>
+                    tomato
+                </button>
+                <button className="btn" style={{backgroundColor:"crimson"}} onClick={() => setColor('crimson')}>
+                    crimson
+                </button>
+                <button className="btn" style={{backgroundColor:"yellow"}} onClick={() => setColor('yellow')}>
+                    yellow
+                </button>
+                <button className="btn" style={{backgroundColor:"purple"}} onClick={() => setColor('purple')}>
+                    purple
+                </button>
+                <button className="btn" style={{backgroundColor:"grey"}} onClick={() => setColor('grey')}>
+                    grey
+                </button>
+                <button className="btn" style={{backgroundColor:"orange"}} onClick={() => setColor('orange')}>
+                    Orange
+                </button>
             </div>
         </div>
     )
